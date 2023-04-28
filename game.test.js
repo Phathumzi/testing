4ed4c7f9f7913131577fbf47845066a094bdd48d
Phathumzi <2129411>
2023-04-28 14:16:05 +0200

    a

--- game.test.js
+++ game.test.js
@@ -1,5 +1,10 @@
 const game = require("./game");
 const game1 = require("./game");
+const game2 = require("./game");
+const game3 = require("./game");
+const game4 = require("./game");
+const game5 = require("./game");
+
 
 
 test("if user is logged in  1 + 2 to equal 3", () => {
@@ -10,3 +15,18 @@ test("if user is logged in  1 + 2 to equal 3", () => {
     expect(game1(1, 2)).toBe(3);
   });
 
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game2(1, 2)).toBe(3);
+  });
+
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game3(1, 2)).toBe(3);
+  });
+
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game4(1, 2)).toBe(3);
+  });
+
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game5(1, 2)).toBe(3);
+  });
