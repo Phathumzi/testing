43e2a6943878b38a6e83aaa1a9b5ac847786e9eb
Phathumzi <2129411>
2023-04-28 13:24:01 +0200

    testing

--- game.test.js
+++ game.test.js
@@ -1,4 +1,9 @@
-const game = require("./game");
+const game = require("./game"), game1 = require("./game") ,game2 = require("./game") , game3 = require("./game"),game4 = require("./game");
+const game5 = require("./game"),game6 = require("./game"),game7 = require("./game"),game8 = require("./game"),game9 = require("./game");
+const game10 = require("./game"),game11 = require("./game"),game12 = require("./game"),game13 = require("./game"),game14 = require("./game");
+const game15 = require("./game"),game16 = require("./game"),game17= require("./game"),game18 = require("./game"),game19 = require("./game");
+const game20 = require("./game"),game21 = require("./game"),game22 = require("./game"),game23 = require("./game"),game24 = require("./game"),game25 = require("./game");
+const game26 = require("./game"),game27 = require("./game"),game28 = require("./game"),game29 = require("./game");
 
 test("if user is logged in  1 + 2 to equal 3", () => {
   expect(game(1, 2)).toBe(3);
