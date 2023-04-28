18a9e2c75ed3d42709d467664215773d53f1e9b3
Phathumzi <2129411>
2023-04-28 13:12:12 +0200

    code cove testing

---
+++ game.test.js
@@ -0,0 +1,86 @@
+const game = require("./game");
+
+test("if user is logged in  1 + 2 to equal 3", () => {
+  expect(game(1, 2)).toBe(3);
+});
+
+test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game1(1, 2)).toBe(3);
+  });
+
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game2(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game3(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game4(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game5(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game6(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game7(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game8(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game9(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game10(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game11(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game12(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game13(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game14(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game15(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game16(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game17(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game18(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game19(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game20(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game21(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game22(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game23(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game24(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game25(1, 2)).toBe(3);
+  });
+  test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game26(1, 2)).toBe(3);
+  });
+
