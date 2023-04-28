e2b5a6ec1f65e69ac87598f35e9ae7f7dc0b9b76
Phathumzi <2129411>
2023-04-28 14:12:36 +0200

    game

--- game.test.js
+++ game.test.js
@@ -1,7 +1,12 @@
-const game = require("./game");const game1 = require("./game");
+const game = require("./game");
+const game1 = require("./game");
 
 
 test("if user is logged in  1 + 2 to equal 3", () => {
   expect(game(1, 2)).toBe(3);
 });
+
+test("if user is logged in  1 + 2 to equal 3", () => {
+    expect(game1(1, 2)).toBe(3);
+  });
 
