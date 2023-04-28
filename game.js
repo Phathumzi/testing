19e48a18c519cf1311fa0a2af39d93266d539607
Phathumzi <2129411>
2023-04-28 14:22:10 +0200

    s

--- game.js
+++ game.js
@@ -7,17 +7,14 @@
   function game2(num1, num2) {
     return num1 + num2;
   }
-  
-  
+    
   function game3(num1, num2) {
     return num1 + num2;
   }
   
-  
   function game4(num1, num2) {
     return num1 + num2;
   }
-  
   
   function game5(num1, num2) {
     return num1 + num2;
@@ -27,8 +24,6 @@
     return num1 + num2;
   }
   
-  
-  
   function game7(num1, num2) {
     return num1 + num2;
   }
@@ -36,7 +31,6 @@
   function game8(num1, num2) {
     return num1 + num2;
   }
-  
   
   function game9(num1, num2) {
     return num1 + num2;
@@ -46,5 +40,4 @@
     return num1 + num2;
   }
   
-
-  module.exports = game,game1,game2,game3,game4,game5,game6,game7,game8,game9,game10,game11,game12,game13,game14,game15,game16,game17,game18,game19,game20,game21,game22,game23,game24;
+  module.exports = game,game1,game2,game3,game4,game5,game6,game7,game8,game9,game10;
