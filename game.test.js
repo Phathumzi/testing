7e79c4a0a520917d626739a708421cb77c7805f7
Phathumzi <2129411>
2023-04-28 13:57:58 +0200

    code cov last attempt

--- game.test.js
+++ game.test.js
@@ -1,25 +1,26 @@
-const game = require("./game"), game1 = require("./game") ,game2 = require("./game") , game3 = require("./game"),game4 = require("./game");
-const game5 = require("./game"),game6 = require("./game"),game7 = require("./game"),game8 = require("./game"),game9 = require("./game");
-const game10 = require("./game"),game11 = require("./game"),game12 = require("./game"),game13 = require("./game"),game14 = require("./game");
-const game15 = require("./game"),game16 = require("./game"),game17= require("./game"),game18 = require("./game"),game19 = require("./game");
-const game20 = require("./game"),game21 = require("./game"),game22 = require("./game"),game23 = require("./game"),game24 = require("./game"),game25 = require("./game");
-const game26 = require("./game"),game27 = require("./game"),game28 = require("./game"),game29 = require("./game");
+const game = require("./game");const game1 = require("./game");
+const game2 = require("./game") ;const game3 = require("./game");const game4 = require("./game");
+const game5 = require("./game");const game6 = require("./game");const game7 = require("./game");const game8 = require("./game");const game9 = require("./game");
+const game10 = require("./game");const game11 = require("./game");const game12 = require("./game");const game13 = require("./game");const game14 = require("./game");
+const game15 = require("./game");const game16 = require("./game");const game17= require("./game");const game18 = require("./game");const game19 = require("./game");
+const game20 = require("./game");const game21 = require("./game");const game22 = require("./game");const game23 = require("./game");const game24 = require("./game");const game25 = require("./game");
+const game26 = require("./game");const game27 = require("./game");const game28 = require("./game");const game29 = require("./game");
 
 test("if user is logged in  1 + 2 to equal 3", () => {
   expect(game(1, 2)).toBe(3);
 });
 
-test1("if user is logged in  1 + 2 to equal 3", () => {
+test("if user is logged in  1 + 2 to equal 3", () => {
     expect(game1(1, 2)).toBe(3);
 });
 
-  test2("if user is logged in  1 + 2 to equal 3", () => {
+  test("if user is logged in  1 + 2 to equal 3", () => {
     expect(game2(1, 2)).toBe(3);
 });
-  test3("if user is logged in  1 + 2 to equal 3", () => {
+  test("if user is logged in  1 + 2 to equal 3", () => {
     expect(game3(1, 2)).toBe(3);
 });
-  test4("if user is logged in  1 + 2 to equal 3", () => {
+  test("if user is logged in  1 + 2 to equal 3", () => {
     expect(game4(1, 2)).toBe(3);
 });
   test5("if user is logged in  1 + 2 to equal 3", () => {
